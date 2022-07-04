b9ac7f4add14a61418a7e86dceab006342544ac3
Hariharasudhan <hari@bassure.com>
2022-07-04 14:37:46 +0530

    wallet topup modified

--- src/main/java/com/fab/adpay/voidService/VoidServiceRequest.java
+++ src/main/java/com/fab/adpay/voidService/VoidServiceRequest.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -23,7 +22,7 @@ public class VoidServiceRequest {
     private String requestRcvTime;
     private String URN;
     private String org_URN;
-    private String loanId   ;
+    private String loanId;
     private String sourceTxnReferenceNo;
     private String nmc;
     private String ageId;
