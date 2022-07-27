0401810ed10a04dfaac17fb59a8e1c4f98a7c6ab
sathiyaseelan T <sathiya@bassure.com>
2022-07-27 20:46:44 +0530

    updated

--- src/main/java/com/fab/adpay/transactionHistory/TransactionHistoryRequest.java
+++ src/main/java/com/fab/adpay/transactionHistory/TransactionHistoryRequest.java
@@ -1,20 +1,20 @@
 package com.fab.adpay.transactionHistory;
 
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
 
 @Data
 @NoArgsConstructor
 @ToString
 public class TransactionHistoryRequest {
 
-    private int requestType;
-    private String cardId;
-    private String startDate;
-    private String endDate;
-    private int numberOfTxns;
-    private int transactionSource;
+	private int requestType;
+	private String value;
+	private String requestMode;
+	private String startDate;
+	private String endDate;
+	private int numberOfTxns;
 
 }
--- src/main/java/com/fab/adpay/transactionHistory/TransactionHistoryResponse.java
+++ src/main/java/com/fab/adpay/transactionHistory/TransactionHistoryResponse.java
@@ -1,23 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.fab.adpay.transactionHistory;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 @ToString
 public class TransactionHistoryResponse {
-
-    private String errorCode;
-    private String errorText;
-    private List<TransactionHistory> transactionHistory;
-
+	private int errorCode;
+	private String errorText;
+	private List<TransactionHistory> transactionHistory;
 }
